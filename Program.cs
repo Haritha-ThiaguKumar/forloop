8c3ae5b6e0780d6496a1c2268ea9b70af9bad1a5
haritha <harithatkumar@gmail.com>
2019-09-10 20:47:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace forloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How many years experience do you have in professional programming");
+            int yearsinsales = 0;
+            yearsinsales = int.Parse(Console.ReadLine());
+            for (int i = 0; i < yearsinsales; i++)
+            {
+                Console.WriteLine("you have {0} years of experience", +yearsinsales);
+            }
+
+
+
+        }
+    }
+}
